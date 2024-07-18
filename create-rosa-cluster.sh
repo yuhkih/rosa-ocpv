4102a98deb5bf298032c612cfea951e952988056
yuhkih <yuhkih@hotmail.com>
2024-07-19 08:51:03 +0900

    test

--- create-rosa-cluster.sh
+++ create-rosa-cluster.sh
@@ -5,6 +5,6 @@ source set-env-rosa.sh
 terraform plan -out rosa.plan
 terraform apply rosa.plan
 echo "====== you can login ROSA with the follwoing comman =====" 
-echo "====== But you would need to wait for a few minuite. 401 would return for a few minuite until the server gets ready"
+echo "====== But you would need to wait for a few minuite.\"Login failed (401 Unauthorized)\" would return for several minuites until the server gets ready"
 echo "oc login -u admin -p " $TF_VAR_admin_password " "$(terraform output -raw cluster_api_url)
 echo "===== End time " `date` " ====="
