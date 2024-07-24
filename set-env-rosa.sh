4a3bfec28b240849a569b372007105afb8b7eba6
yuhkih <yuhkih@hotmail.com>
2024-07-24 09:34:50 +0900

    remove an environmental value

--- set-env-rosa.sh
+++ set-env-rosa.sh
@@ -4,7 +4,7 @@ export TF_VAR_cluster_name="$(whoami)-hcp"
 export TF_VAR_admin_password="$(whoami)-Passw0rd12345!"
 export TF_VAR_developer_password="$(whoami)-Passw0rd12345!"
 export CLUSTER=$TF_VAR_cluster_name
-export CLUSTER_API_URL="$(terraform output -raw cluster_api_url)"
+# export CLUSTER_API_URL="$(terraform output -raw cluster_api_url)"
 
 echo "===== Set the follwoing environment variables ====="
 echo "TF_VAR_token="$TF_VAR_token
@@ -12,4 +12,4 @@ echo "TF_VAR_cluster_name="$TF_VAR_cluster_name
 echo "TF_VAR_admin_password="$TF_VAR_admin_password
 echo "TF_VAR_developer_password="$TF_VAR_developer_password
 echo "CLUSTER="$CLUSTER
-echo "CLUSTER_API_URL="$CLUSTER_API_URL
+# echo "CLUSTER_API_URL="$CLUSTER_API_URL
